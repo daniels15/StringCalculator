fe1581c429bf16df1a1901539926b9cf17648171
daniels15 <daniels15@ru.is>
2017-10-13 22:43:47 +0000

    Implement and test for empty input string

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,8 +2,15 @@ package is.ru.stringcalculator;
 
 public class Calculator
 {
-	public static int Add(string numbers)
+	public static int Add(String numbers)
 	{
-
+		if (numbers.equals(""))
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,5 +5,9 @@ import org.junit.Test;
 
 public class CalculatorTest
 {
-	
+	@Test
+	public void testEmptyString()
+	{
+		assertEquals(0, Calculator.Add(""));
+	}
 }
